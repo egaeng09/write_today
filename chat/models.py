31d3bbefb63c8a4fb1edf69129c07af8c2ddb64f
egaeng <egaeng09@gmail.com>
2024-10-08 05:26:43 +0900

    test

--- chat/models.py
+++ chat/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.apps import apps
+from diary.models import Member
 
 class Room(models.Model):
-    counselor = models.ForeignKey('diary.Member', related_name='counselor_room', on_delete=models.CASCADE)
-    client = models.ForeignKey('diary.Member', related_name='client_room', on_delete=models.CASCADE)
+    counselor = models.ForeignKey(Member, related_name='counselor_room', on_delete=models.CASCADE)
+    client = models.ForeignKey(Member, related_name='client_room', on_delete=models.CASCADE)
     is_opened = models.BooleanField(default = False)
 
     is_active = models.BooleanField(default = True)
@@ -13,7 +13,7 @@ class Room(models.Model):
 
 
 class CounselorInfo(models.Model):
-    member = models.ForeignKey('diary.Member', on_delete = models.CASCADE)
+    member = models.ForeignKey(Member, on_delete = models.CASCADE)
     propensity = models.BooleanField(default = False)
     summary = models.TextField(null = False)
 
@@ -37,8 +37,8 @@ class Chat(models.Model):
     
 
 class Match(models.Model):
-    counselor = models.ForeignKey('diary.Member', related_name='counselor', on_delete=models.CASCADE)
-    client = models.ForeignKey('diary.Member', related_name='client', on_delete=models.CASCADE)
+    counselor = models.ForeignKey(Member, related_name='counselor', on_delete=models.CASCADE)
+    client = models.ForeignKey(Member, related_name='client', on_delete=models.CASCADE)
     # 우울감이 존재하는 사람과 상담사를 매칭
     # 상담사 입장에서 우울감을 느끼는 사용자 LIST 출력 > 상담사가 채팅을 시작하면 MATCH에서 사라지고 ROOM으로 변경
     # 사용자 입장에서 우울감 느낄 시 상담사 1인 매칭 > 채팅 시작하면 동일하게 ROOM으로
