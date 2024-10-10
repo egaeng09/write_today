572803b94f439776a58889d05d54ffdf7179e881
egaeng <egaeng09@gmail.com>
2024-10-10 16:22:50 +0900

    cosr

--- write_today/settings.py
+++ write_today/settings.py
@@ -164,6 +164,7 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://localhost:5500',
+    'http://127.0.0.1:5500',
 ] # CORS 허용 URL
 
 AUTH_USER_MODEL = 'diary.Member'
